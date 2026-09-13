ff2fab7f11c412d47a982fad972b4d2d8b6ab961
lakshanmenaka <menakalakshan92@gmail.com>
2026-09-13 16:17:29 +0530

    Change CORS origin to 'http://goopiy.online'

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'http://goopiy.online',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     })
